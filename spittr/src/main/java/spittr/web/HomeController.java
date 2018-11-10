d3490e8970416459c24ad1f588e9901edfafaecc
ducnha <daoducnha29493@gmail.com>
2018-11-10 11:57:10 +0700

    Add unit test for homecontroller

--- spittr/src/main/java/spittr/web/HomeController.java
+++ spittr/src/main/java/spittr/web/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
+@RequestMapping({"/","/homepage"})
 public class HomeController {
-    @RequestMapping(value="/", method=GET)
+    @RequestMapping(method=GET)
     public String home() {
         return "home";
     }
