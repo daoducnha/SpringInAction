52bd902c9c21a7b88d3ac8ef55cf23e68bc9d556
ducnha <daoducnha29493@gmail.com>
2018-11-11 07:30:51 +0700

    Commit Form Registration

---
+++ spittr/src/main/java/spittr/data/impl/SpitterRepositoryImpl.java
@@ -0,0 +1,14 @@
+package spittr.data.impl;
+
+import org.springframework.stereotype.Component;
+import spittr.Spitter;
+import spittr.data.SpitterRepository;
+
+@Component
+public class SpitterRepositoryImpl implements SpitterRepository {
+
+  @Override
+  public Spitter save(Spitter spitter) {
+    return null;
+  }
+}
--- spittr/src/main/test/spittr/web/SpitterControllerTest.java
+++ spittr/src/main/test/spittr/web/SpitterControllerTest.java
@@ -3,6 +3,8 @@ package spittr.web;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
+import spittr.Spitter;
+import spittr.data.SpitterRepository;
 
 import static
     org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -15,8 +17,25 @@ public class SpitterControllerTest {
 
   @Test
   public void shouldShowRegistration() throws Exception {
-    SpitterController controller = new SpitterController();
+    SpitterRepository repository = Mockito.mock(SpitterRepository.class);
+    SpitterController controller = new SpitterController(repository);
     MockMvc mockMvc = standaloneSetup(controller).build();
     mockMvc.perform(get("/spitter/register")).andExpect(view().name("registerForm"));
+  }
+
+  @Test
+  public void shouldProcessRegistration() throws Exception{
+    SpitterRepository mockreRepository = Mockito.mock(SpitterRepository.class);
+    Spitter unsaved =
+        new Spitter("jbauer", "24hours", "Jack", "Bauer");
+    Spitter saved =
+        new Spitter(24L, "jbauer", "24hours", "Jack", "Bauer");
+    Mockito.when(mockreRepository.save(unsaved)).thenReturn(saved);
+    SpitterController controller = new SpitterController(mockreRepository);
+    MockMvc mockMvc = standaloneSetup(controller).build();
+    mockMvc.perform(post("/spitter/register").param("firstName", "Jack").param("lastName", "Bauer")
+        .param("username", "jbauer")
+        .param("password", "24hours")).andExpect(redirectedUrl("/spitter/jbauer"));
+    Mockito.verify(mockreRepository, Mockito.atLeastOnce()).save(unsaved);
   }
 }
